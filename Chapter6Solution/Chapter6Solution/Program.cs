b707e15b9e276c5acd6dfb6c0f137e0326f4382e
Irame <irame94@gmail.com>
2017-06-24 14:27:27 +0200

    Added Benchmark

--- Chapter6Solution/Chapter6Solution/Program.cs
+++ Chapter6Solution/Chapter6Solution/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,38 +28,72 @@ namespace Chapter6Solution
             problem = parsedProblem;
             Console.WriteLine($"Problem: n={problem.Variables.Count}, m={problem.Clauses.Count}");
 
+            int benchmarkRuns = 1000;
+
             int trys = 100;
+
 
             Console.WriteLine();
             Console.WriteLine("Algorithmus A:");
             AlgorithmA algoA = new AlgorithmA();
+
             int satisfiedClausesA = algoA.GenerateAssingment(problem, trys, out VariableAssignment assignmentA);
             Console.WriteLine($"Anzahl der Versuche: {trys}");
             Console.WriteLine($"Erfüllte Klauseln: {satisfiedClausesA}");
+
+            TimeSpan algoAExecutionTime = Benchmark(benchmarkRuns, () => algoA.GenerateAssingment(problem, trys, out assignmentA));
+            Console.WriteLine($"Average Runtime for {benchmarkRuns} runs: {algoAExecutionTime.TotalMilliseconds:##0.#####} ms");
 
 
             Console.WriteLine();
             Console.WriteLine("Algorithmus B:");
             var algoB = new AlgorithmB(parsedProblem);
             algoB.GenerateProbabilities();
+
             int satisfiedClausesB = algoB.GenerateAssingment(problem, trys, out VariableAssignment assignmentB);
             Console.WriteLine($"Anzahl der Versuche: {trys}");
             Console.WriteLine($"Erfüllte Klauseln: {satisfiedClausesB}");
 
+            TimeSpan algoBExecutionTime = Benchmark(benchmarkRuns, () =>
+            {
+                algoB.GenerateProbabilities();
+                algoB.GenerateAssingment(problem, trys, out assignmentB);
+            });
+            Console.WriteLine($"Average Runtime for {benchmarkRuns} runs: {algoBExecutionTime.TotalMilliseconds:##0.#####} ms");
+
 
             Console.WriteLine();
             Console.WriteLine("Algorithmus DRAND_A:");
+
             int satisfiedClausesDrandA = ArgorithmDrand.GenerateAssignment(problem, algoA, out VariableAssignment assignmentDrandA);
             Console.WriteLine($"Erfüllte Klauseln: {satisfiedClausesDrandA}");
+
+            TimeSpan algoDerandAExecutionTime = Benchmark(benchmarkRuns, () => ArgorithmDrand.GenerateAssignment(problem, algoA, out assignmentDrandA));
+            Console.WriteLine($"Average Runtime for {benchmarkRuns} runs: {algoDerandAExecutionTime.TotalMilliseconds:##0.#####} ms");
 
 
             Console.WriteLine();
             Console.WriteLine("Algorithmus DRAND_B:");
+
             int satisfiedClausesDrandB = ArgorithmDrand.GenerateAssignment(problem, algoB, out VariableAssignment assignmentDrandB);
             Console.WriteLine($"Erfüllte Klauseln: {satisfiedClausesDrandB}");
 
+            TimeSpan algoDerandBExecutionTime = Benchmark(benchmarkRuns, () => ArgorithmDrand.GenerateAssignment(problem, algoB, out assignmentDrandB));
+            Console.WriteLine($"Average Runtime for {benchmarkRuns} runs: {algoDerandBExecutionTime.TotalMilliseconds:##0.#####} ms");
+
 
             Console.Read();
+        }
+
+        private static TimeSpan Benchmark(int n, Action f)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < n; i++)
+            {
+                f();
+            }
+            sw.Stop();
+            return new TimeSpan(sw.ElapsedTicks / n);
         }
     }
 }
